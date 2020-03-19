36a363518c1b634f6b35fdc2832819b0374a1fc3
jamesm0rr1s <34630328+jamesm0rr1s@users.noreply.github.com>
2020-03-19 14:46:08 -0400

    Add files via upload

---
+++ show-tilix-keyboard-shortcuts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set an alias
+# alias th='bash /opt/jamesm0rr1s/Tilix-Kali-Setup/show-tilix-keyboard-shortcuts.sh'
+
+# Alias name
+# th == tilix help
+
+# Print the Tilix keyboard shortcuts
+# th
+
+# Print the keyboard commands
+cat /opt/jamesm0rr1s/Tilix-Kali-Setup/keyboard-commands.txt
